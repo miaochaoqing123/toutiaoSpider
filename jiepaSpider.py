4c65e420870e9a5b04820f082703e44de6d930c8
缪超青 <419666193@qq.com>
2018-04-28 13:36:38 +0800

    分析Ajax请求并抓取今日头条街拍美图

---
+++ jiepaSpider.py
@@ -0,0 +1,33 @@
+import requests
+from urllib.parse import urlencode
+from requests.exceptions import RequestException
+
+def get_page_index():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36"
+    }
+    data = {
+        'offset': 0,
+        'format': 'json',
+        'keyword': '街拍',
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': 1,
+        'from': 'search_tab'
+    }
+    url = 'https://www.toutiao.com/search_content/?' + urlencode(data)
+    response = requests.get(url=url,headers=headers)
+    try:
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        print('请求索引页出错')
+        return None
+
+
+
+
+
+
+
